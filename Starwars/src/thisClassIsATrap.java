dfa2beeb5fdea3717689787fb182ec7842df80ba
cdurham2417 <cdurham2417@001LAB153010829.neisd.ad>
2015-10-20 15:44:04 -0500

    Strike me down with all of your commitment.

---
+++ Starwars/src/thisClassIsATrap.java
@@ -0,0 +1,7 @@
+public class thisClassIsATrap {
+	public static void main(String[] args) {
+		System.out.println("Break off the attack! The compiler's still up!");
+		System.out.println("Now witness the power of this fully operational Java code!");
+		System.out.println("That println came from the Java code! That things's operational!");
+	}
+}
